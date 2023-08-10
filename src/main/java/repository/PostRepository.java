5e0d4492f5877ffa59b2eeb92d9ce1b4ce603d0b
Dixie Dean <d.w.sokolov26@gmail.com>
2023-08-10 12:40:35 +0300

    Refactored PostRepository

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -2,24 +2,32 @@ package repository;
 
 import model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-// Stub
 public class PostRepository {
+  private final List<Post> postList = new ArrayList<>();
+
   public List<Post> all() {
-    return Collections.emptyList();
+    return postList;
   }
 
   public Optional<Post> getById(long id) {
+    for (Post post : postList) {
+      if (post.getId() == id) {
+        return Optional.of(post);
+      }
+    }
     return Optional.empty();
   }
 
   public Post save(Post post) {
+    postList.add(post);
     return post;
   }
 
   public void removeById(long id) {
+      postList.removeIf(post -> post.getId() == id);
   }
 }
